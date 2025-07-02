b83fea5393b1b76da526147fc1b4ff056c9bbf4f
Arman Ihsan <armanihsan224@gmail.com>
2025-07-02 08:30:58 +0500

    authentication implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
-
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -10,9 +10,16 @@ const Navbar = () => {
           <Image src={"/images/logo.svg"} width={46} height={44} alt={"logo"} />
         </div>
       </Link>{" "}
-      <div className={"flex gap-8 items-center"}>
+      <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign in</p>
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
